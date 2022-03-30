ba7b0c9a0d35b8d975050645880cee1029daa64d
Hongrui Duo <duohongrui@cqnu.edu.cn>
2022-03-30 17:37:47 +0800

    add new function make_tree

--- R/traj_utils.R
+++ R/traj_utils.R
@@ -249,3 +249,97 @@ match_cells <- function(ref_data, sim_data){
               'cell_pair'=cell_pair))
 
 }
+
+
+
+
+
+
+
+
+
+#' Convert An Expression Matrix into Tree Format
+#'
+#' This function is used to convert the gene expression matrix into the tree
+#' format.
+#'
+#'
+#' @param ref_data A Matrix. The gene expression matrix where every row represents
+#' a cell and every column represents a gene.
+#' @param group A Vector. The group information of the cells in ref_data
+#' @param is_Newick If TRUE, return the Newick format.
+#' @param is_parenthetic If TRUE, return the parenthetic format.
+#' @return A file of parenthetic format or Newick format
+#' @export
+#'
+#' @examples
+#' ref_data <- matrix(rpois(n = 2500, lambda = 2), nrow = 50)
+#' rownames(ref_data) <- paste0("cell_", 1:ncol(ref_data))
+#' colnames(ref_data) <- paste0("gene_", 1:nrow(ref_data))
+#' group <- c(rep("A", 15), rep("B", 5), rep("C", 20), rep("D", 10))
+#' tree_format <- make_tree(ref_data, group = group)
+make_tree <- function(ref_data, group, is_Newick=TRUE, is_parenthetic=FALSE){
+
+  data <- Seurat::CreateSeuratObject(counts = ref_data, verbose=FALSE)
+
+  data <- Seurat::NormalizeData(data,
+                                normalization.method = "LogNormalize",
+                                scale.factor = 10000,
+                                verbose=FALSE)
+
+  data <- Seurat::FindVariableFeatures(data,
+                                       selection.method = "vst",
+                                       nfeatures = 2000,
+                                       verbose=FALSE)
+
+  all.genes <- rownames(data)
+
+  data <- Seurat::ScaleData(data, features = all.genes, verbose=FALSE)
+
+  data@meta.data$'group' <- stringr::str_remove_all(group, pattern = "[(].+[)]")
+
+  #Get state tree by hierachical clustering on the state means
+  exp_data <- Seurat::AverageExpression(data, slot = 'scale.data', group.by = 'group')
+
+  clu <- stats::hclust(stats::dist(t(exp_data$RNA)), method = 'ward.D')
+
+
+  for(i in 1:length(clu[["labels"]])){
+
+    clu[["labels"]][i] <- stringr::str_replace_all(clu[["labels"]][i], ",", "_")
+
+  }
+
+  phyla <- ctc::hc2Newick(clu, flat=TRUE)
+
+  if(is_Newick){
+
+    return(phyla)
+
+  }
+
+  phyla <- ape::read.tree(text = phyla)
+
+  if(is_parenthetic){
+
+    return(list(phyla))
+
+  }
+
+  phyla$edge.length <- ceiling(phyla$edge.length)
+
+  phyla$edge.length[phyla$edge.length == 0] <- 1
+
+  return(phyla)
+
+}
+
+
+
+
+
+
+
+
+
+
