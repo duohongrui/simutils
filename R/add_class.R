cb3d87b77cdc5ae9df130108a02d4ff6164b183b
Hongrui Duo <duohongrui@cqnu.edu.cn>
2022-04-17 11:29:01 +0800

    add functions of parameter-defining

---
+++ R/add_class.R
@@ -0,0 +1,17 @@
+#' Add A New Class
+#'
+#' @param x An object.
+#' @param define_class A new self-defined class
+#' @export
+#'
+#' @examples
+#' a <- matrix(rnorm(1000), nrow = 100)
+#' a <- add_class(a, 'my_matrix')
+add_class <- function(x, define_class){
+  base::class(x) <- c(base::class(x), define_class)
+  x
+}
+
+
+
+
