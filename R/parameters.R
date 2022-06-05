3e4933a4199ef21672c562adcd8681190cc52a73
duohongrui <duohongrui@cqnu.edu.cn>
2022-06-05 22:51:12 +0800

    fix parameters function

--- R/parameters.R
+++ R/parameters.R
@@ -118,11 +118,9 @@ param_numeric <- function(id,
   }
 
   if(is.null(lower)){
-    cat(id,":", "The parameter lower is not defined and will be set to -Inf\n")
     lower <- -Inf
   }
   if(is.null(upper)){
-    cat(id,":", "The parameter upper is not defined and will be set to Inf\n")
     upper <- Inf
   }
   if(!is.infinite(lower) & !is.infinite(upper)){
