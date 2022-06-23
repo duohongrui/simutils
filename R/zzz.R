f1bc208146931ea5f3cd40584bdd946fed9109a3
duohongrui <duohongrui@cqnu.edu.cn>
2022-06-23 10:46:03 +0800

    fix onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,3 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to simutils")
-  suppressPackageStartupMessages({
-    require(powsimR)
-    require(rtracklayer)
-  })
 }
