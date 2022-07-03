e1131b9a168fcbc8bf1e06f98554d443250a4a2f
duohongrui <duohongrui@cqnu.edu.cn>
2022-07-03 10:22:46 +0800

    add check_prior_info function

---
+++ R/check_prior_info.R
@@ -0,0 +1,31 @@
+#' @export
+check_prior_info <- function(
+    method,
+    step,
+    other_prior,
+    er
+){
+  ## group.condition
+  if(step == "estimation"){
+    group_con_method <- c("Lun2",
+                          "scDD",
+                          "zingeR",
+                          "SparseDC")
+  }
+  if(step == "simulation"){
+    group_con_method <- c("zinbwaveZinger",
+                          "zingeR")
+  }
+
+  if(method %in% group_con_method){
+    if(is.null(other_prior[["group.condition"]])){
+      stop(paste0("Please input group condition information when you use ", method))
+    }else{
+      other_prior_exec <- other_prior
+      other_prior_exec[["group.condition"]] <- other_prior[["group.condition"]][[er]]
+    }
+  }else{
+    other_prior_exec <- other_prior
+  }
+  return(other_prior_exec)
+}
