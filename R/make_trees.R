c0ae3c5ccbb9ea853db62b0c2e3255626293cb47
duohongrui <duohongrui@cqnu.edu.cn>
2022-07-06 20:45:41 +0800

    fix make_trees function

--- R/make_trees.R
+++ R/make_trees.R
@@ -12,7 +12,7 @@
 #' @return A file of parenthetic format or Newick format
 #' @export
 #' @importFrom  Seurat CreateSeuratObject NormalizeData FindVariableFeatures
-#' ScaleData AverageExpression
+#' ScaleData AverageExpression RunPCA FindNeighbors FindClusters VariableFeatures
 #' @importFrom stringr str_remove_all str_replace_all
 #' @importFrom stats hclust dist
 #'
@@ -22,56 +22,45 @@
 #' colnames(ref_data) <- paste0("gene_", 1:nrow(ref_data))
 #' group <- c(rep("A", 15), rep("B", 5), rep("C", 20), rep("D", 10))
 #' tree_format <- make_trees(ref_data, group = group)
-make_trees <- function(ref_data, group, is_Newick=TRUE, is_parenthetic=FALSE){
-
-  data <- CreateSeuratObject(counts = t(ref_data), verbose=FALSE)
-
-  data <- NormalizeData(data,
-                        normalization.method = "LogNormalize",
-                        scale.factor = 10000,
-                        verbose=FALSE)
-
-  data <- FindVariableFeatures(data,
-                               selection.method = "vst",
-                               nfeatures = 2000,
-                               verbose=FALSE)
+make_trees <- function(ref_data,
+                       group = NULL,
+                       is_Newick=TRUE,
+                       is_parenthetic=FALSE){
+  data <- Seurat::CreateSeuratObject(counts = t(ref_data), verbose=FALSE)
+  data <- Seurat::NormalizeData(data,
+                                normalization.method = "LogNormalize",
+                                scale.factor = 10000,
+                                verbose=FALSE)
+  data <- Seurat::FindVariableFeatures(data,
+                                       selection.method = "vst",
+                                       nfeatures = 2000,
+                                       verbose=FALSE)
   all.genes <- rownames(data)
-
-  data <- ScaleData(data, features = all.genes, verbose=FALSE)
-
-  data@meta.data$'group' <- str_remove_all(group, pattern = "[(].+[)]")
-
+  data <- Seurat::ScaleData(data, features = all.genes, verbose=FALSE)
+  if(is.null(group)){
+    data <- Seurat::RunPCA(data, features = Seurat::VariableFeatures(object = data), verbose = FALSE)
+    data <- Seurat::FindNeighbors(data, dims = 1:10)
+    data <- Seurat::FindClusters(data, resolution = 0.5, verbose = FALSE)
+    group_num <- length(unique(data@meta.data[["seurat_clusters"]]))
+    cat(paste0("Your data has ", group_num, " groups \n"))
+    group <- paste0("group", as.numeric(data@meta.data[["seurat_clusters"]]))
+  }
+  data@meta.data$'group' <- stringr::str_remove_all(group, pattern = "[(].+[)]")
   #Get state tree by hierachical clustering on the state means
-  exp_data <- AverageExpression(data, slot = 'scale.data', group.by = 'group')
-
-  clu <- hclust(dist(t(exp_data$RNA)), method = 'ward.D')
-
+  exp_data <- Seurat::AverageExpression(data, slot = 'scale.data', group.by = 'group')
+  clu <- stats::hclust(stats::dist(t(exp_data$RNA)), method = 'ward.D')
   for(i in 1:length(clu[["labels"]])){
-
-    clu[["labels"]][i] <- str_replace_all(clu[["labels"]][i], ",", "_")
-
+    clu[["labels"]][i] <- stringr::str_replace_all(clu[["labels"]][i], ",", "_")
   }
-
   phyla <- ctc::hc2Newick(clu, flat=TRUE)
-
   if(is_Newick){
-
     return(phyla)
-
   }
-
   phyla <- ape::read.tree(text = phyla)
-
   if(is_parenthetic){
-
     return(list(phyla))
-
   }
-
   phyla$edge.length <- ceiling(phyla$edge.length)
-
   phyla$edge.length[phyla$edge.length == 0] <- 1
-
   return(phyla)
-
 }
