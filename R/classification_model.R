f2c6d40e17aa7c1cbfe24693333c4f37fe8d32bb
duohongrui <duohongrui@duohongruideMacBook-Pro.local>
2022-08-12 21:59:22 +0800

    fix model_predict function

--- R/classification_model.R
+++ R/classification_model.R
@@ -5,15 +5,34 @@
 #
 # result <- simutils::perform_DEA(data = data, group = group, method = "edgeRQLF")
 # de_genes <- rownames(result)[result$FDR < 0.05]
-
+#
+#
+# estimate_result <- simmethods::Splat_estimation(ref_data = data, verbose = TRUE, seed = 111)
+# prob.group = as.numeric(table(group)/ncol(data))
+# de.prob = length(de_genes)/nrow(data)
+# simulate_result <- simmethods::Splat_simulation(
+#   estimate_result$estimate_result,
+#   other_prior = list(prob.group = c(0.3, 0.4, 0.3),
+#                      de.prob = 0.3),
+#   return_format = "list",
+#   verbose = TRUE,
+#   seed = 111)
+#
+# data <- simulate_result$simulate_result$count_data
+# col_data <- simulate_result$simulate_result$col_meta
+# group <- col_data$group
+# row_data <- simulate_result$simulate_result$row_meta
+# de_genes <- rownames(row_data)[row_data$de_gene == "yes"]
 
 #' Establish A Model And Predict Using DEGs
 #'
 #' @param data A matrix with cells on columns and genes on rows.
 #' @param group Group(or cluster) assignment of every cells in columns of matrix.
 #' @param de_genes A character vector of DEGs.
-#' @param method The method to establish the model. SVM, Decision tree.
+#' @param method The method to establish the model. SVM, Decision tree or RF (Random Forest).
 #' @importFrom stats predict
+#' @importFrom caret confusionMatrix
+#' @importFrom pROC roc multiclass.roc
 #' @export
 model_predict <- function(
   data,
@@ -33,7 +52,7 @@ model_predict <- function(
   train_data <- data[train_index, intersect(de_genes, names(gene_var)[gene_var != 0])]
   test_data <- data[-train_index, intersect(de_genes, names(gene_var)[gene_var != 0])]
   ## group information
-  group <- as.numeric(as.factor(group))
+  group <- as.factor(group)
   train_group <- group[train_index]
   test_group <- group[-train_index]
 
@@ -42,16 +61,28 @@ model_predict <- function(
     if(!requireNamespace("e1071", quietly = TRUE)){
       stop("Package \"e1071\" must be installed by \"install.packages('e1071')\" command.")
     }
+    message("Modeling by SVM...")
     svm_classifier <- e1071::svm(x = train_data,
-                                 y = train_group,
+                                 y = as.factor(train_group),
                                  cross = 10,
+                                 probability = TRUE,
                                  kernel = 'radial',
-                                 prob = TRUE,
                                  scale = FALSE)
-    svm_predict <- stats::predict(svm_classifier,
-                                  as.matrix(test_data),
-                                  prob = TRUE)
-    return(svm_predict)
+    message("Predicting...")
+    predict_class <- stats::predict(svm_classifier,
+                                    as.matrix(test_data),
+                                    prob = FALSE)
+    conf_matrix <- caret::confusionMatrix(predict_class, test_group)
+    predict_prob <- stats::predict(svm_classifier,
+                                   as.matrix(test_data),
+                                   prob = TRUE)
+    if(nlevels(group) == 2){
+      roc <- pROC::roc(response = test_group,
+                       predictor = attr(predict_prob, "probabilities")[, 1])
+    }else{
+      roc <- pROC::multiclass.roc(response = test_group,
+                                  predictor = attr(predict_prob, "probabilities")[, 1])
+    }
   }
 
   if(method == "Decision tree"){
@@ -63,18 +94,57 @@ model_predict <- function(
     tree_model <- rpart::rpart(group ~ .,
                                data = train_data,
                                method = "class")
-    tree_predict <- stats::predict(tree_model,
+    predict_class <- stats::predict(tree_model,
+                                    as.data.frame(test_data),
+                                    type = "class")
+    conf_matrix <- caret::confusionMatrix(predict_class, test_group)
+    predict_prob <- stats::predict(tree_model,
                                    as.data.frame(test_data),
-                                   prob = TRUE)
-    return(tree_predict)
+                                   type = "prob")
+    if(nlevels(group) == 2){
+      roc <- pROC::roc(response = test_group,
+                       predictor = predict_prob[, 1])
+    }else{
+      roc <- pROC::multiclass.roc(response = test_group,
+                                  predictor = predict_prob[, 1])
+    }
   }
-}
 
+  if(method == "RF"){
+    if(!requireNamespace("randomForest", quietly = TRUE)){
+      stop("Package \"randomForest\" must be installed by \"install.packages('randomForest')\" command.")
+    }
+    train_data$group <- train_group
+    colnames(train_data) <- gsub(pattern = "-",
+                                 replacement = "",
+                                 colnames(train_data))
+    colnames(test_data) <- gsub(pattern = "-",
+                                 replacement = "",
+                                 colnames(test_data))
+    RF_model <- randomForest::randomForest(as.factor(group) ~ .,
+                                           data = train_data)
+    predict_class <- stats::predict(RF_model,
+                                    test_data,
+                                    type = "class")
+    conf_matrix <- caret::confusionMatrix(predict_class, test_group)
+    predict_prob <- stats::predict(RF_model,
+                                   test_data,
+                                   type = "prob")
+    if(nlevels(group) == 2){
+      roc <- pROC::roc(response = test_group,
+                       predictor = predict_prob[, 1])
+    }else{
+      roc <- pROC::multiclass.roc(response = test_group,
+                                  predictor = predict_prob[, 1])
+    }
+  }
+  return(dplyr::lst(conf_matrix, roc))
+}
 
 
 # par(pty = "s")
 # print(pROC::roc(test_group,
-#                 tree_predict[,2],
+#                 RF_predict_prob[, 2],
 #                 plot = TRUE,
 #                 legacy.axes = TRUE,
 #                 percent = TRUE,
@@ -83,4 +153,7 @@ model_predict <- function(
 #                 xlab = "FPR",
 #                 ylab = "TPR",
 #                 lwd = 3))
-# roc_obj <- pROC::roc(test_group, svm_predict)
+# roc_obj <- pROC::roc(test_group, RF_predict_prob[, 2])
+
+
+
